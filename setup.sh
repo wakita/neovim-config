bdf29616a0c3b40eb38442ea81b49ed70012bf9d
Ken Wakita <wakita@is.titech.ac.jp>
2019-03-23 13:11:10 +0900

    vimtex

--- setup.sh
+++ setup.sh
@@ -16,5 +16,15 @@ if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+'''
+Python 3 support:
+    conda activate py37
+    conda install unidecode
+    pip install pynvim neovim-remote
+
+NodeJS support:
+    npm install -g neovim
+'''
+
 # Create local temporary directories
 mkdir -p $HOME/.tmp/vim/{backup,undo}
---
+++ user/tex.vim
@@ -0,0 +1,12 @@
+" Gilles Castel: How I'm able to take notes in mathematics lectures using LaTeX and Vim
+" https://castel.dev/post/lecture-notes-1
+
+" NeoVim report support
+let g:vimtex_compiler_progname = 'nvr'
+
+let g:tex_flavor='latex'
+"let g:vimtex_view_method='zathura'
+let g:vimtex_view_method='skim'
+let g:vimtex_quickfix_mode=0
+set conceallevel=1
+let g:tex_conceal='abdmg'
